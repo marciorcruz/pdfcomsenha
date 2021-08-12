5e660941b544fe809897a1506810a597b801e4ec
Marcio Ribeiro da Cruz <marciorcruz@hotmail.com>
2021-08-12 13:53:14 -0300

    Create Projeto

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.IO;
+using iTextSharp.text.pdf;
+
+namespace pdfcode
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //"/Users/macbookpro/Documents/BUILD/pdfcode/test.pdf"
+            //"/Users/macbookpro/Documents/BUILD/pdfcode/test_encrypted.pdf"
+            //pdfcode.dll "/Users/macbookpro/Documents/BUILD/pdfcode/test.pdf" "test_e2.pdf" "secret"
+            //dotnet publish -f net5.0 -r win10-x64 --self-contained false
+
+            if (args.Length != 3)
+            {
+                Console.WriteLine("Está faltando dados!");
+                return ;
+            }
+
+            if (args[2].Length <= 0)
+            {
+                Console.WriteLine("Senha não pode ser vazia");
+                return ;
+            }
+
+            if (Program.IsValidPdf(args[0]))
+            {
+                using (Stream input = new FileStream(args[0], FileMode.Open, FileAccess.Read, FileShare.Read))
+                using (Stream output = new FileStream(args[1], FileMode.Create, FileAccess.Write, FileShare.None))
+                {
+                    PdfReader reader = new PdfReader(input);
+                    PdfEncryptor.Encrypt(reader, output, true, args[2].ToString(), args[2].ToString(), PdfWriter.ALLOW_PRINTING);
+                    Console.WriteLine("Arquivo com senha ( " + args[1] + " )");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Arquivo PDF Inválido");
+            }
+        }
+
+        private static bool IsValidPdf(string filepath)
+        {
+            bool Ret = true;
+            PdfReader reader = null;
+            try
+            {
+                reader = new PdfReader(filepath);
+            }
+            catch
+            {
+                Ret = false;
+            }
+            return Ret;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # PDF CODE
 Criar arquivo PDF com SENHA em .NET CORE 5.0
 
+
+## USE
+
+dotnet add package iTextSharp.LGPLv2.Core --version 1.7.3
+
 ## Como usar
 //pdfcode.exe "/Users/myuser/test.pdf" "/Users/myuser/test_com_senha.pdf" "sua_senha"
 
@@ -8,6 +13,8 @@ Criar arquivo PDF com SENHA em .NET CORE 5.0
 git clone https://github.com/marciorcruz/pdfcomsenha.git
 
 cd pdfcomsenha
+
+dotnet restore
 
 dotnet build
 
